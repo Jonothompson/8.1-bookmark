ec1e797fbe15d0a9795da02c7053015047ce4122
Jonathan Thompson <JonathanLeighThompson@gmail.com>
2015-06-22 13:36:23 -0400

    after deleting 4 repo's I now have this working correctly up the childview stage

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,15 @@
-(function(){
-  'use strict';
-
-  $(document).ready(function(){
-    $('body').prepend(JST.application());
-  });
-})();
+import BookmarkListView from './views/bookmarkListView';
+ (function(){
+   'use strict';
+ 
+   $(document).ready(function(){
+     var bookmarks = new Backbone.Collection([
+       {title: "Google", url: "http://google.com"},
+       {title: "Twitter", url: "http://twitter.com"},
+       {title: "Cool", url: "http://cool.com"},
+     ]);
+ 
+    var listView = new BookmarkListView({collection: bookmarks});
+    $('body').prepend(listView.el);
+   });
+ })();
