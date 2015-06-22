eadd013cde8e39e9bc12bf49f6d4ce15c0fa4746
Jonathan Thompson <JonathanLeighThompson@gmail.com>
2015-06-22 13:40:14 -0400

    updating bookmarkItemView and pushing branch jt-develop ahead of master

--- scripts/views/bookmarkItemView.js
+++ scripts/views/bookmarkItemView.js
@@ -2,6 +2,10 @@ export default Backbone.View.extend({
   template: JST.index,
 
   tagName: 'li',
+  
+  events: {
+    'click .bookmark-edit': 'editBookmark'
+  },
 
   initialize: function(){
     this.render();
@@ -9,5 +13,10 @@ export default Backbone.View.extend({
 
   render: function(){
     this.$el.html(this.template(this.model.toJSON()));
+  },
+  
+  editBookmark: function(e) {
+    console.log('hello');
   }
+  
 });
